2618ca35d9142dc85af5434d167f8d0e1d90c04e
maythazinsoe <tzsakura@gmail.com>
2015-10-10 15:30:25 +0900

    2015.10.10の授業内容

--- app/src/main/java/com/example/student/study/MainActivity.java
+++ app/src/main/java/com/example/student/study/MainActivity.java
@@ -4,19 +4,59 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private ListView mListStudy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListStudy = (ListView) findViewById(R.id.listStudy);
+        List<String> studyList = new ArrayList<String>();
+        studyList.add("Service");
+        studyList.add("ContentProvider");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+        studyList.add("Sqlite3");
+
+        ListAdapter adapter = new ArrayAdapter<String>
+                (this,android.R.layout.simple_list_item_1,studyList);
+        mListStudy.setAdapter(adapter);
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
